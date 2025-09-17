4d41e7d602ca9bd442780c730dabac5ab14473f1
Gonzalo Atienza <gonatienza@gmail.com>
2025-09-17 15:33:02 -0400

    conjur-jwt

--- src/ark_api/authorizations/__init__.py
+++ src/ark_api/authorizations/__init__.py
@@ -1,4 +1,4 @@
 # flake8: noqa F401
 from .basic import Basic
-from .bearer import PlatformBearer, AppBearer, ConjurBearer
+from .bearer import PlatformBearer, AppBearer, ConjurBearer, JwtBearer
 from .apikey import FlowsApiKey, ConjurApiKey
--- src/ark_api/tokens/workloadtoken.py
+++ src/ark_api/tokens/workloadtoken.py
@@ -5,27 +5,48 @@ from urllib.parse import quote
 
 
 class ConjurWorkloadToken(ConjurToken):
-    _API_PATH_FORMAT = (
+    _API_PATH_FORMAT_API_KEY = (
         "https://{}.secretsmgr.cyberark.cloud/api/"
         "authn/conjur/{}/authenticate"
     )
+    _API_PATH_FORMAT_AUHTENTICATOR = (
+        "https://{}.secretsmgr.cyberark.cloud/api/"
+        "authn-jwt/{}/conjur/authenticate"
+    )
 
-    def __init__(self, auth, subdomain, identifier):
-        verify(auth, "ConjurApiKey", "auth must be ConjurApiKey")
+    def __init__(self, auth, subdomain, identifier, authenticator=""):
+        verify(
+            auth,
+            ["ConjurApiKey", "JwtBearer"],
+            "auth must be ConjurApiKey or JwtBearer"
+        )
+        verify(auth, "JwtBearer", "auth must be JwtBearer")
         verify(subdomain, "str", "subdomain must be str")
         verify(identifier, "str", "identifier must be str")
+        verify(authenticator, "str", "authenticator must be str")
         self._subdomain = subdomain
-        api_path = self._API_PATH_FORMAT.format(
-            self._subdomain,
-            quote(f"host/{identifier}", safe="")
-        )
         headers = {"Accept-Encoding": "base64"}
-        data = SecretBytes(auth.api_key.get().encode())
+        if auth.__class__.__name__ == "ConjurApiKey":
+            api_path = self._API_PATH_FORMAT_API_KEY.format(
+                self._subdomain,
+                quote(f"host/{identifier}", safe=""),
+            )
+            data = SecretBytes(auth.api_key.get().encode())
+            params = {}
+        elif auth.__class__.__name__ == "JwtBearer":
+            api_path = self._API_PATH_FORMAT_AUHTENTICATOR.format(
+                self._subdomain,
+                authenticator
+            )
+            headers.update({"Content-Type": "x-www-form-urlencoded"})
+            params = {"jwt": auth.token.access_token.get()}
+            data = b""
         method = "POST"
         self._response = api_call(
             api_path=api_path,
             method=method,
             headers=headers,
+            params=params,
             data=data
         )
         super().__init__()
