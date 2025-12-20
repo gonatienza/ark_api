39b8266036ec8e25b6a403ed70109d9479529585
Gonzalo Atienza <gonatienza@gmail.com>
2025-12-20 18:15:51 -0500

    addsafemembers

--- src/ark_api/safes/safes.py
+++ src/ark_api/safes/safes.py
@@ -4,13 +4,19 @@ from ark_api.utils import verify
 
 
 class Safes(ArkApiCall):
-    _API_PATH_FORMAT = (
-        "https://{}.privilegecloud.cyberark.cloud/PasswordVault/API/Safes/"
+    __API_PATH_FORMAT = (
+        "https://{}.privilegecloud.cyberark.cloud/PasswordVault/"
     )
+    _API_PATH_FORMAT = __API_PATH_FORMAT + "API/Safes/"
 
-    def __init__(self, auth):
+    def __init__(self, auth, next_link=None):
         verify(auth, "PlatformBearer", "auth must be PlatformBearer")
-        api_path = self._API_PATH_FORMAT.format(auth.token.subdomain)
+        if next_link:
+            verify(next_link, "str", "next_link must be str")
+            _api_path = self.__API_PATH_FORMAT.format(auth.token.subdomain)
+            api_path = _api_path + next_link
+        else:
+            api_path = self._API_PATH_FORMAT.format(auth.token.subdomain)
         params = {
             "includeAccounts": True,
             "extendedDetails": True
