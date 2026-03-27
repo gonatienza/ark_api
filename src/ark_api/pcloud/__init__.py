b86e39fcf97c128b07a3b73589227f603f2af70d
gon <gonatienza@gmail.com>
2026-03-26 20:23:59 -0400

    update

--- src/ark_api/pcloud/__init__.py
+++ src/ark_api/pcloud/__init__.py
@@ -2,3 +2,4 @@
 from .ipallowlist import GetIpAllowList, SetIpAllowList
 from .safemembers import SafeMembers, AddSafeMembers
 from .safes import Safes
+from .platforms import Platforms, GetPlatform
---
+++ src/ark_api/pcloud/platforms.py
@@ -0,0 +1,38 @@
+from ark_api.model import ArkApiCall
+from ark_api.utils import api_call
+from ark_api.utils import verify
+
+
+class Platforms(ArkApiCall):
+    _API_PATH_FORMAT = (
+        "https://{}.privilegecloud.cyberark.cloud/PasswordVault/API/Platforms/"
+    )
+
+    def __init__(self, auth):
+        verify(auth, "PlatformBearer", "auth must be PlatformBearer")
+        api_path = self._API_PATH_FORMAT.format(auth.token.subdomain)
+        headers = {
+            **auth.header,
+            "Content-Type": "application/json"
+        }
+        method = "GET"
+        response = api_call(api_path, method, headers)
+        self._response = response.json()
+
+
+class GetPlatform(ArkApiCall):
+    _API_PATH_FORMAT = (
+        "https://{}.privilegecloud.cyberark.cloud/PasswordVault/API/Platforms/{}"
+    )
+
+    def __init__(self, auth, platform_id):
+        verify(auth, "PlatformBearer", "auth must be PlatformBearer")
+        verify(platform_id, "str", "auth must be str")
+        api_path = self._API_PATH_FORMAT.format(auth.token.subdomain, platform_id)
+        headers = {
+            **auth.header,
+            "Content-Type": "application/json"
+        }
+        method = "GET"
+        response = api_call(api_path, method, headers)
+        self._response = response.json()
